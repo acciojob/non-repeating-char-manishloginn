e3c62fc85585bc9c21ca1e9d938ac745c8100a81
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-12 10:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const charCount = new Map();
+ 
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+ 
+  for (const [char, count] of charCount) {
+    if (count === 1) {
+      return char;
+    }
+  }
+ 
+  return null;
 }
+ 
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
